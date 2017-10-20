f2c0c095e4ac492c1248a8336f9ff1312789186a
aswanipu <aswanipu@gmail.com>
2017-10-20 14:19:47 +0530

    Grid Layout and date and time

---
+++ WorkingDemoLayout/WorkingDemoLayout/WorkingDemoLayout/View/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkingDemoLayout.ViewModel;
+using Xamarin.Forms;
+
+namespace WorkingDemoLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel vm;
+        public MainPage()
+        {
+            InitializeComponent();
+            //DateLabel.Text = DateTime.Now.ToFileTimeUtc().ToString();
+
+            DateLabel.Text = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff");
+            vm = new MainPageViewModel();
+            BindingContext= vm;
+        }
+    }
+}
